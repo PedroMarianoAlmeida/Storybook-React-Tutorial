ae1836b3795f1b0908181bdfe6da3c5b750a0c2b
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-29 14:10:01 -0300

    Theming

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { addDecorator } from '@storybook/react';
-import Center from './../src/components/center/Center';
+import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
 
-addDecorator(story => <Center>{story()}</Center>)
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <CSSReset />
+    <Box m='4'>
+      {story()}
+    </Box>
+  </ThemeProvider>))
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
