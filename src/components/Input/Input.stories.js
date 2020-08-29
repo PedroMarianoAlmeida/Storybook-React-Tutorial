c4766fea4a062850a5b7ae45a7038926182833df
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-08-29 13:46:32 -0300

    Global Decorator

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Input from './Input';
-import Center from '../center/Center'
 
 export default {
     title: 'My Test/Input',
     Component: Input,
-    decorators: [story => <Center>{story()}</Center>]
 }
 
 export const Small = () => <Input variant='small' placeholderText='small' />
